94cc8f76076b33ceadd8e66859aa0ba0b8f27444
RICHARD DAROS <richard.daros@bravosul.com.br>
2022-03-03 08:59:59 -0300

    Add test Dashborad and App.tsx

--- src/pages/Dashboard/__tests__/Dashboard.test.tsx
+++ src/pages/Dashboard/__tests__/Dashboard.test.tsx
@@ -2,10 +2,8 @@ import renderer from 'react-test-renderer'
 
 import { Dashboard } from '../index'
 
-describe('Dashboard', () => {
-  test('Snapshot Dashboard', () => {
-    const component = renderer.create(<Dashboard />)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+it('Snapshot Dashboard', () => {
+  const component = renderer.create(<Dashboard />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
