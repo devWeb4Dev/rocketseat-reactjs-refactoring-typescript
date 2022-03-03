30cb56e15b138e208f7f19a800d54a401419ff06
RICHARD DAROS <richard.daros@bravosul.com.br>
2022-03-03 08:39:43 -0300

    Snapshot Dashboard

--- src/pages/Dashboard/__tests__/Dashboard.test.tsx
+++ src/pages/Dashboard/__tests__/Dashboard.test.tsx
@@ -2,8 +2,8 @@ import renderer from 'react-test-renderer'
 
 import { Dashboard } from '../index'
 
-describe('Counter', () => {
-  test('Link changes the class when hovered', () => {
+describe('Dashboard', () => {
+  test('Snapshot Dashboard', () => {
     const component = renderer.create(<Dashboard />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
